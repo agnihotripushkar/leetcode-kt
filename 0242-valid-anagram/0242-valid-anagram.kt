d575023b81c8812b31f20c97b298018a0d3b794c
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-06-11 21:59:03 -0700

    Time: 20 ms (56.28%), Space: 45.7 MB (58.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -1,30 +1,18 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-        val len1 = s.length
-        val len2 = t.length
+        if (s.length !=t.length) return false
+        val map = hashMapOf<Char,Int>()
 
-        if(len1!=len2){
-            return false
-        }
-        val map1 = hashMapOf<Char,Int>()
-        val map2 = hashMapOf<Char,Int>()
-
-        for (i in 0 until len1){
-            map1[s[i]] = map1.getOrDefault(s[i],0)+1
+        s.forEach{char ->
+            map[char] = map.getOrDefault(char,0)+1
         }
 
-        for (j in 0 until len2){
-            map2[t[j]] = map2.getOrDefault(t[j],0)+1
+        t.forEach{
+            val count = map[it]
+            if (count ==0 || count ==null) return false
+            else map[it] = count-1
         }
 
-        if (map1.size!=map2.size){
-            return false
-        }
-        for ((word,count) in map1){
-            if (map1[word]!=map2[word]){
-                return false
-            }
-        }
         return true
         
     }
