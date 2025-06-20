a77d6dfe0191b4d8afc3e8ab9275dcbddb9ad748
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-06-20 15:23:07 -0500

    Time: 5 ms (21.97%), Space: 64.6 MB (92.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
@@ -1,15 +1,13 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
-        if(prices.isEmpty()){
-            return 0
-        }
-        var minVal = prices[0]
         var maxProfit = 0
+        var minPrice = prices[0]
 
-        for(i in 1 until prices.size){
-            minVal = Math.min(minVal, prices[i])
-            maxProfit = Math.max(maxProfit, prices[i]-minVal)
+        for (i in 1 until prices.size){
+            minPrice = Math.min(minPrice,prices[i])
+            maxProfit = Math.max(maxProfit, prices[i]-minPrice)
         }
+        
         return maxProfit
     }
 }
