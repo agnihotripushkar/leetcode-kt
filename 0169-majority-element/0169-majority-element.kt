77c8891663696a8e628282a5a70fb74c96522e0b
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-07-10 05:49:46 -0500

    Time: 2 ms (92.44%), Space: 56.5 MB (24.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.kt
+++ 0169-majority-element/0169-majority-element.kt
@@ -1,7 +1,21 @@
 class Solution {
     fun majorityElement(nums: IntArray): Int {
-        nums.sort()
-        val n = nums.size
-        return nums[n/2]
+        var candidate = 0
+        var count = 0
+
+        for (num in nums){
+            if (count==0){
+                candidate = num
+            }
+            if (num==candidate){
+                count++
+            }
+            else{
+                count--
+            }
+        }
+
+        return candidate
+        
     }
 }
