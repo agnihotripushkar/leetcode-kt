b3a4f3142934226715ea36119cc1f5942cbb7f72
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-07-14 22:12:55 -0500

    Time: 0 ms (100%), Space: 42.5 MB (24.57%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        var poi1 = m-1
+        var poi2 = n-1
+        var index = m+n-1
+
+        while(poi2>=0){
+            if(poi1>=0 && nums1[poi1]>nums2[poi2]){
+                nums1[index--] = nums1[poi1--]
+            }
+            else{
+                 nums1[index--] = nums2[poi2--]
+            }
+        }
+    }
+}
