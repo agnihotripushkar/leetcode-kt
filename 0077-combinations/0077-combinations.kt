b30d1f4dd28c6dcade61b36dc76588e9cdd35692
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-08-25 15:03:08 -0700

    Time: 21 ms (90.4%), Space: 116.5 MB (18.64%) - LeetHub

---
+++ 0077-combinations/0077-combinations.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun combine(n: Int, k: Int): List<List<Int>> {
+        val result = ArrayList<List<Int>>()
+        var curr = ArrayList<Int>()
+
+        fun dfs(start:Int){
+            if(curr.size == k){
+                result.add(ArrayList(curr))
+                return 
+            }
+
+            for(i in start .. n){
+                curr.add(i)
+                dfs(i+1)
+                curr.removeAt(curr.size-1)
+            }
+        }
+
+        dfs(1)
+        return result
+        
+    }
+}
