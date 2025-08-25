09ace570d724f1782f1b3edfd9ae04fff2a6a280
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-08-25 15:03:52 -0700

    Time: 19 ms (93.79%), Space: 116.2 MB (20.9%) - LeetHub

--- 0077-combinations/0077-combinations.kt
+++ 0077-combinations/0077-combinations.kt
@@ -3,7 +3,7 @@ class Solution {
         val result = ArrayList<List<Int>>()
         val curr = ArrayList<Int>()
 
-        fun dfs(start:Int){
+        fun backtrack(start:Int){
             if(curr.size == k){
                 result.add(ArrayList(curr))
                 return 
@@ -11,12 +11,12 @@ class Solution {
 
             for(i in start .. n){
                 curr.add(i)
-                dfs(i+1)
+                backtrack(i+1)
                 curr.removeAt(curr.size-1)
             }
         }
 
-        dfs(1)
+        backtrack(1)
         return result
         
     }
