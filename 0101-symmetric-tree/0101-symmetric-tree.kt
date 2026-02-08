62934066f7d57ed8dcf7caa6271bbb29f735fdfe
Pushkar Aagnihotri <agnihotripush@gmail.com>
2026-02-07 17:36:22 -0800

    Time: 0 ms (100%), Space: 41.5 MB (82.3%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        if(root==null) return true
+        return isMirror(root.left,root.right)  
+    }
+
+    fun isMirror(node1: TreeNode?,node2: TreeNode?):Boolean{
+        if(node1==null && node2==null) return true
+        if (node1 == null || node2 == null) return false
+
+        return node1.`val` == node2.`val` 
+            && isMirror(node1.left, node2.right) 
+            && isMirror(node1.right, node2.left)
+    }
+}
