27e9cad6926a6c3743260fc37c18aa39ebb135ae
Pushkar Aagnihotri <agnihotripush@gmail.com>
2026-04-09 14:36:40 -0700

    Time: 0 ms (100%), Space: 49.9 MB (31.25%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.kt
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.kt
@@ -1,28 +1,15 @@
 class Solution {
     fun diagonalSum(mat: Array<IntArray>): Int {
-        val rows = mat.size
+        val size = mat.size
         var sum = 0
-        val list = mutableSetOf<Pair<Int,Int>>()
 
-        for(i in 0 until rows){
-            for(j in 0 until rows){
-                 if(i==j && !list.contains(Pair(i,j))){
-                    list.add(Pair(i,j))
-                    println(mat[i][j])
-                    sum += mat[i][j]
-                 }
-            }
+        for(i in 0 until size){
+            sum += mat[i][i]
+            sum += mat[i][size-1-i]
         }
-        println("sum = ${sum}")
 
-        for(i in rows-1 downTo 0){
-            for(j in 0 until rows){
-                if(i+j==(rows-1) && !list.contains(Pair(i,j))){
-                    list.add(Pair(i,j))
-                    println("${mat[i][j]} with $i and $j")
-                    sum += mat[i][j]
-                }
-            }
+        if(size%2!=0){
+           sum  -= mat[size/2][size/2]
         }
 
         return sum
